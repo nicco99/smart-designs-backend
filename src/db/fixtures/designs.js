614c49a918f55e9d6e0e662d63c0ced5be609a3b
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-06-11 01:04:40 +0300

    update the seeds

--- src/db/fixtures/designs.js
+++ src/db/fixtures/designs.js
@@ -1,24 +1,336 @@
-module.exports = [ {
-  no_of_bathrooms: 2,
-  no_of_bedrooms: 3,
-  county: "Example County",
-  location: "Example Location",
-  image1: "https://example.com/image1.jpg",
-  image2: "https://example.com/image2.jpg",
-  image3: "https://example.com/image3.jpg",
-  image4: "https://example.com/image4.jpg",
-  image5: "https://example.com/image5.jpg",
-  property_size: "2000 sq ft",
-  property_type: "Example Property Type",
-  property_name: "Example Property Name",
-  price_per_sqm: 150,
-  total_price: 300000,
-  class_of_finishes: "High-end",
-  feature1: "Feature 1",
-  feature2: "Feature 2",
-  feature3: "Feature 3",
-  feature4: "Feature 4",
-  feature5: "Feature 5",
-  status: "completed",
-}  
+module.exports = [
+  {
+    no_of_bathrooms: 2,
+    no_of_bedrooms: 3,
+    county: "Nairobi",
+    location: "Ngong rd",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+    image4: "https://example.com/image4.jpg",
+    image5: "https://example.com/image5.jpg",
+    property_size: "50000 sq ft",
+    property_type: "Bungalow",
+    property_name: "3 Bedroom",
+    price_per_sqm: 150,
+    total_price: 3000000,
+    class_of_finishes: "High-end",
+    feature1: "Kitchen with Pantry",
+    feature2: "Master bathroom Bathtub",
+    feature3: "Entry poches",
+    feature4: "Enclosed one car parking",
+    feature5: "imported American fittings",
+    status: "completed",
+  },
+  //done1
+
+  {
+    no_of_bathrooms: 3,
+    no_of_bedrooms: 4,
+    county: "Kiambu",
+    location: "Ruiru",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_MAST_TP7_OP1-4271-FRONT.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_MAST_TP7_OP1-4271-REAR.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_MAST_TP7_OP1-4271-REAR.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_MAST_TP7_OP1-4271-REAR.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_MAST_TP7_OP1-4271-REAR.jpg.webp&w=1920&q=50",
+    property_size: "31000 sq ft",
+    property_type: "Maisonnette",
+    property_name: "4 Bedroom",
+    price_per_sqm: 150,
+    total_price: 4500000,
+    class_of_finishes: "High-end",
+    feature1: "Dinning area",
+    feature2: "Family room",
+    feature3: "Entry poches",
+    feature4: "Water connection",
+    feature5: "Open plan kitchen",
+    status: "completed",
+  },
+  //done2
+
+  {
+    no_of_bathrooms: 3,
+    no_of_bedrooms: 4,
+    county: "Nairobi",
+    location: "langata rd",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_BNGL_TP4.1_OP1_REAR.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4BED_BNGL_TP4.1_OP2_INTERIOR.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_BNGL_TP4.1_OP1_FRONT.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_BNGL_TP4.1_OP1_FRONT.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4_BED_BNGL_TP4.1_OP1_FRONT.jpg.webp&w=1920&q=50",
+    property_size: "60000 sq ft",
+    property_type: "Bungalow",
+    property_name: "3 Bedroom",
+    price_per_sqm: 100,
+    total_price: 5000000,
+    class_of_finishes: "High-end",
+    feature1: "Kitchen with Pantry",
+    feature2: "Master bathroom Bathtub",
+    feature3: "Entry poches",
+    feature4: "Enclosed one car parking",
+    feature5: "imported American fittings",
+    status: "completed",
+  },
+  //done3
+
+  {
+    no_of_bathrooms: 3,
+    no_of_bedrooms: 4,
+    county: "Kirinyage",
+    location: "Mwea",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3171_Back_P2LLiB9.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3171_Front_M7xFH1o.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3171_Back_P2LLiB9.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3171_Back_P2LLiB9.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3171_Back_P2LLiB9.jpg.webp&w=1920&q=50",
+    property_size: "50000 sq ft",
+    property_type: "Maisonette",
+    property_name: "4 Bedroom",
+    price_per_sqm: 150,
+    total_price: 6000000,
+    class_of_finishes: "High-end",
+    feature1: "Home Gym",
+    feature2: "Rooftop Terrace",
+    feature3: "Home Theater",
+    feature4: "Wine Cellar",
+    feature5: "Outdoor Fireplace",
+    status: "completed",
+  },
+  //done4
+
+  {
+    no_of_bathrooms: 3,
+    no_of_bedrooms: 4,
+    county: "Machakos",
+    location: "Wote",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4291_front_K4aDGcD.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4291_MFB6Ylr.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4291_front_K4aDGcD.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F4291_front_K4aDGcD.jpg.webp&w=1920&q=50",
+    property_size: "50000 sq ft",
+    property_type: "Maisonette ",
+    property_name: "4 Bedroom",
+    price_per_sqm: 150,
+    total_price: 3000000,
+    class_of_finishes: "High-end",
+    feature1: "Imported American Fittings",
+    featur2: "Rooftop Terrace",
+    feature3: "Home Theater",
+    feature4: "Wine Cellar",
+    feature5: "Outdoor Fireplace",
+  },
+  //done5
+
+  {
+    no_of_bathrooms: 2,
+    no_of_bedrooms: 3,
+    county: "Muranga",
+    location: "Maragua",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2FD5_Front.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2FD5_Front.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2FD5_Front.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2FD5_Front.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2FD5_Front.jpg.webp&w=1920&q=50",
+    property_size: "50000 sq ft",
+    property_type: "Bungalow",
+    property_name: "3 Bedroom",
+    price_per_sqm: 150,
+    total_price: 3000000,
+    class_of_finishes: "High-end",
+    feature1: "Kitchen with Pantry",
+    feature2: "Master bathroom Bathtub",
+    feature3: "Entry poches",
+    feature4: "Enclosed one car parking",
+    feature5: "imported American fittings",
+    status: "completed",
+  },
+  //done6
+  {
+    no_of_bathrooms: 2,
+    no_of_bedrooms: 3,
+    county: "Nairobi",
+    location: "Westland",
+    image1:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F31181_Back.jpg.webp&w=1920&q=50",
+    image2:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F31181_Front.jpg.webp&w=1920&q=50",
+    image3:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F31181_Front.jpg.webp&w=1920&q=50",
+    image4:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F31181_Back.jpg.webp&w=1920&q=50",
+    image5:
+      "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F31181_Back.jpg.webp&w=1920&q=50",
+    property_size: "50000 sq ft",
+    property_type: "Bungalow",
+    property_name: "3 Bedroom",
+    price_per_sqm: 150,
+    total_price: 3000000,
+    class_of_finishes: "High-end",
+    feature1: "Kitchen with Pantry",
+    feature2: "Master bathroom Bathtub",
+    feature3: "Entry poches",
+    feature4: "Enclosed one car parking",
+    feature5: "imported American fittings",
+    status: "completed",
+  },
+  // {
+  //   no_of_bathrooms: 2,
+  //   no_of_bedrooms: 3,
+  //   county: "Nairobi",
+  //   location: "Ngong rd",
+  //   image1:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+  //   image2:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+  //   image3:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+  //   image4: "https://example.com/image4.jpg",
+  //   image5: "https://example.com/image5.jpg",
+  //   property_size: "50000 sq ft",
+  //   property_type: "Bungalow",
+  //   property_name: "3 Bedroom",
+  //   price_per_sqm: 150,
+  //   total_price: 3000000,
+  //   class_of_finishes: "High-end",
+  //   feature1: "Kitchen with Pantry",
+  //   feature2: "Master bathroom Bathtub",
+  //   feature3: "Entry poches",
+  //   feature4: "Enclosed one car parking",
+  //   feature5: "imported American fittings",
+  //   status: "completed",
+  // },
+  // {
+  //   no_of_bathrooms: 2,
+  //   no_of_bedrooms: 3,
+  //   county: "Nairobi",
+  //   location: "Ngong rd",
+  //   image1:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+  //   image2:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+  //   image3:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+  //   image4: "https://example.com/image4.jpg",
+  //   image5: "https://example.com/image5.jpg",
+  //   property_size: "50000 sq ft",
+  //   property_type: "Bungalow",
+  //   property_name: "3 Bedroom",
+  //   price_per_sqm: 150,
+  //   total_price: 3000000,
+  //   class_of_finishes: "High-end",
+  //   feature1: "Kitchen with Pantry",
+  //   feature2: "Master bathroom Bathtub",
+  //   feature3: "Entry poches",
+  //   feature4: "Enclosed one car parking",
+  //   feature5: "imported American fittings",
+  //   status: "completed",
+  // },
+  // {
+  //   no_of_bathrooms: 2,
+  //   no_of_bedrooms: 3,
+  //   county: "Nairobi",
+  //   location: "Ngong rd",
+  //   image1:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+  //   image2:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+  //   image3:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+  //   image4: "https://example.com/image4.jpg",
+  //   image5: "https://example.com/image5.jpg",
+  //   property_size: "50000 sq ft",
+  //   property_type: "Bungalow",
+  //   property_name: "3 Bedroom",
+  //   price_per_sqm: 150,
+  //   total_price: 3000000,
+  //   class_of_finishes: "High-end",
+  //   feature1: "Kitchen with Pantry",
+  //   feature2: "Master bathroom Bathtub",
+  //   feature3: "Entry poches",
+  //   feature4: "Enclosed one car parking",
+  //   feature5: "imported American fittings",
+  //   status: "completed",
+  // },
+  // {
+  //   no_of_bathrooms: 2,
+  //   no_of_bedrooms: 3,
+  //   county: "Nairobi",
+  //   location: "Ngong rd",
+  //   image1:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+  //   image2:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+  //   image3:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+  //   image4: "https://example.com/image4.jpg",
+  //   image5: "https://example.com/image5.jpg",
+  //   property_size: "50000 sq ft",
+  //   property_type: "Bungalow",
+  //   property_name: "3 Bedroom",
+  //   price_per_sqm: 150,
+  //   total_price: 3000000,
+  //   class_of_finishes: "High-end",
+  //   feature1: "Kitchen with Pantry",
+  //   feature2: "Master bathroom Bathtub",
+  //   feature3: "Entry poches",
+  //   feature4: "Enclosed one car parking",
+  //   feature5: "imported American fittings",
+  //   status: "completed",
+  // },
+  // {
+  //   no_of_bathrooms: 2,
+  //   no_of_bedrooms: 3,
+  //   county: "Nairobi",
+  //   location: "Ngong rd",
+  //   image1:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Front.jpg.webp&w=1920&q=50",
+  //   image2:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Back.jpg.webp&w=1920&q=50",
+  //   image3:
+  //     "https://www.inuua.net/_next/image?url=%2Fimages%2Ftmp%2FplanImages%2F3152_Interior.jpg.webp&w=1920&q=50",
+  //   image4: "https://example.com/image4.jpg",
+  //   image5: "https://example.com/image5.jpg",
+  //   property_size: "50000 sq ft",
+  //   property_type: "Bungalow",
+  //   property_name: "3 Bedroom",
+  //   price_per_sqm: 150,
+  //   total_price: 3000000,
+  //   class_of_finishes: "High-end",
+  //   feature1: "Kitchen with Pantry",
+  //   feature2: "Master bathroom Bathtub",
+  //   feature3: "Entry poches",
+  //   feature4: "Enclosed one car parking",
+  //   feature5: "imported American fittings",
+  //   status: "completed",
+  // },
 ];
