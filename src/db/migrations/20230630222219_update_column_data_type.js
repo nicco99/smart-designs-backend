c60c9f74f1d42396ff014eb82e034894b422b283
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-07-01 09:33:19 +0300

    done

--- src/db/migrations/20230630222219_update_column_data_type.js
+++ src/db/migrations/20230630222219_update_column_data_type.js
@@ -2,16 +2,14 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
 exports.up = function (knex) {
-    return knex.schema.alterTable('designs', function (table) {
-      table.integer('plinth_area');
-    });
-  };
+  return knex.schema.alterTable("designs", function (table) {
+    table.integer("plinth_area").alter();
+  });
+};
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
--- src/db/seeds/00-designs.js
+++ src/db/seeds/00-designs.js
@@ -1,13 +1,12 @@
+const data = require("../fixtures/designs");
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("designs").truncate();
+
+  // Inserts seed entries
+  await knex("designs").insert(data);
 };
